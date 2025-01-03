22894999cc8b24ca52f37b9ffb221a71d1d4054a
Achmad Fathoni <fathoni.id@gmail.com>
2025-01-03 18:15:52 +0700

    Maybe just downgrade ipython

--- flake.nix
+++ flake.nix
@@ -56,7 +56,7 @@
           pip install .[jupyter]
           pip install jedi-language-server
           pip install -e .
-          pip install --upgrade ipython prompt-toolkit
+          pip install ipython==8.12.0
         '';
       };
     });
