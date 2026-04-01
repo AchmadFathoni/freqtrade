b3fc93d9cf009a04786252e4d970875ae5fec5f7
Achmad Fathoni <fathoni.id@gmail.com>
2026-04-01 21:40:28 +0700

    Simplify flake.nix

--- flake.nix
+++ flake.nix
@@ -1,36 +1,22 @@
-# Run
-#
-#   nix develop .#setupShell
-#
-# to install packages via pip initially (and stay in shell).
-# Deletes your `.venv`!
-#
-# Run
-#
-#   nix develop
-#
-# to get a development shell afterwards.
-
 {
   description = "Freqtrade crypto trading bot";
   inputs = {
     # nixpkgs.url = "nixpkgs/nixos-25.05";
     nixpkgs.url = "git+file:///home/toni/Documents/source/nixpkgs";
-    flake-utils.url = "github:numtide/flake-utils";
   };
   outputs = {
     nixpkgs,
-    flake-utils,
     ...
   }:
-    flake-utils.lib.eachDefaultSystem (system: let
+    let
+      system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
       buildInputs = [
         (pkgs.python3.withPackages (python-pkgs: [
           python-pkgs.pip
           python-pkgs.virtualenv
         ]))
-        pkgs.zlib
+
         # Matplotlib run dependency
         pkgs.glib
         pkgs.zlib
@@ -43,27 +29,29 @@
         pkgs.xorg.libX11
       ];
     in {
-      devShells.default = pkgs.mkShell {
-        inherit buildInputs;
-        shellHook = ''
-          export LD_LIBRARY_PATH=${pkgs.stdenv.cc.cc.lib}/lib/
-          export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath buildInputs}:$LD_LIBRARY_PATH"
-          source .venv/bin/activate
-        '';
+      devShells.${system} = {
+        default = pkgs.mkShell{
+          inherit buildInputs;
+          shellHook = ''
+            export LD_LIBRARY_PATH="${pkgs.stdenv.cc.cc.lib}/lib:${pkgs.lib.makeLibraryPath buildInputs}"
+            source .venv/bin/activate
+          '';
+        };
+
+        setupSHell = pkgs.mkShell {
+          inherit buildInputs;
+          #TODO: make requirements-tony.txt
+          shellHook = ''
+            export LD_LIBRARY_PATH=${pkgs.stdenv.cc.cc.lib}/lib/
+            rm -rf .venv
+            virtualenv --no-setuptools .venv
+            source .venv/bin/activate
+            pip install -r requirements-dev.txt
+            pip install -r requirements-tony.txt
+            pip install .[jupyter]
+            pip install -e .
+          '';
+        };
       };
-      devShells.setupShell = pkgs.mkShell {
-        inherit buildInputs;
-        #TODO: make requirements-tony.txt
-        shellHook = ''
-          export LD_LIBRARY_PATH=${pkgs.stdenv.cc.cc.lib}/lib/
-          rm -rf .venv
-          virtualenv --no-setuptools .venv
-          source .venv/bin/activate
-          pip install -r requirements-dev.txt
-          pip install -r requirements-tony.txt
-          pip install .[jupyter]
-          pip install -e .
-        '';
-      };
-    });
+    };
 }
